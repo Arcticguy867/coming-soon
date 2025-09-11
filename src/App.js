7d51335295e85e89cbeb0d35a50f126f9bf72de8
Arcticguy867 <sidmit867@gmail.com>
2025-09-11 13:09:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LiquidEther from './LiquidEther';
+import DecryptedText from './DecryptedText'; // Import DecryptedText
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: "#060010", height: "100vh", width: "100vw" }}>
+      <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+        <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+
+        <div className="content">
+          <h1>
+            <DecryptedText text="Coming Soon" />
+          </h1>
+          <p>
+            <DecryptedText text="Something amazing is on the way!" />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
